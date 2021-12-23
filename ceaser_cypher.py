4ea2afa46453be7e5b36b9a415ad105ecd7532ff
vaibhav200225 <89749277+vaibhav200225@users.noreply.github.com>
2021-12-24 00:29:22 +0530

    Add files via upload

--- ceaser_cypher.py
+++ ceaser_cypher.py
@@ -1,3 +1,4 @@
+import random
 import pyfiglet
 import colorama
 from colorama import *
@@ -11,11 +12,13 @@ print(Fore.GREEN + Style.BRIGHT + "Created on 18-12-2021\n")
 
 def RESTART():
 
-    ALPHABET = (' :A+1B<2C>D3]E4/F[5G?6H!I7J@8K}L{9M#NO$0PQ%R^ST&U~VW*X&YZ')
-    KEY = 3
+    ALPHABET = (' :A+1B<2C>D3]E4/F[5G?6H!I7J@8K}L{9M#NO$0PQ%R^ST&U~V*WX&YZ')
+    VALUE = random.randint(0, len(ALPHABET))
+    KEY = int(VALUE)
 
-    print(Fore.GREEN + '[-]'+ Fore.BLUE +"1.Only encryption\n")
+    print(Fore.GREEN + '[-]'+ Fore.BLUE + "1.Only encryption\n")
     print(Fore.GREEN + '[-]'+ Fore.BLUE + "2.Both encryp/decryp\n")
+    print(Fore.GREEN + '[-]'+ Fore.BLUE + "3.For brute-force the encryption\n")
     Choice = int(input(Fore.CYAN + "Choose any one of above: "))
 
     if(Choice==1):
@@ -67,6 +70,23 @@ def RESTART():
             print(Fore.GREEN + decrypted)
         CONTINUE()
 
+    elif(Choice==3):
+        
+        NEW_ENCRYPTION = str(input(Fore.CYAN +"Please enter the text that you have to BRUTE-FORCE: \n"))
+        def ceaser_crack(cipher_text):
+             for key in range (len(ALPHABET)):
+                 plain_text = ''
+
+                 for c in cipher_text:
+                    index =ALPHABET.find(c)
+                    index = (index-key)%len(ALPHABET)
+                    plain_text = plain_text + ALPHABET[index]
+                 print(Fore.GREEN + "[-] With key %s, the result is: %s"%(key,plain_text))
+                
+        if __name__ == "__main__":
+            encrypted = NEW_ENCRYPTION
+            ceaser_crack(encrypted)
+        CONTINUE()
 
     else:
         print(Fore.GREEN + Style.BRIGHT + "You have entered invalid option!")
@@ -76,8 +96,14 @@ def CONTINUE():
     restart=str(input(Fore.CYAN + "Do you want to restart the programe?(YES/NO):\n").lower())
     
     if(restart == 'yes'):
+        print()
         RESTART()
 
+    elif(restart == 'no'):
+        print(Fore.GREEN + "THANK YOU for using this programme!!")
+
     else:
+        print(Fore.GREEN + "invalid syntac!!")
         exit
+        
 RESTART()
